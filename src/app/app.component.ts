3a27373d882551a715401329dfcca5e4aebc00a5
Shaina Koval <skoval@aiimi.com>
2019-09-27 15:35:14 +0100

    🦄 Set up changeable css variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'theme-experiment';
+  primary: string;
+  secondary: string;
+
+  ngOnInit() {
+    this.primary = '6369d1';
+    this.secondary = '60e1e0';
+  }
+
+  updateColor() {
+    const css =
+      ' :root {' +
+      '--primary: #' +
+      this.primary +
+      ';' +
+      '--secondary: #' +
+      this.secondary +
+      ';' +
+      '}';
+    const style = document.getElementById('cssVars');
+    style.innerHTML = css;
+  }
 }
